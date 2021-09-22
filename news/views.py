0627a9aa3582bd12061c42d1dcf8ada9bda99867
angl0826 <75575109+angl0826@users.noreply.github.com>
2021-09-22 18:44:17 +0900

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView
+
+from .filters import PostFilter  # импортируем недавно написанный фильтр
+from .models import Post, Category
+from .forms import PostForm
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-id')
+    paginate_by = 10
+
+    def get_filter(self):
+        return PostFilter(self.request.GET, queryset=super().get_queryset())
+
+    def get_queryset(self):
+        return self.get_filter().qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+
+        context['categories'] = Category.objects.all()
+        context['form'] = PostForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)  # создаём новую форму, забиваем в неё данные из POST-запроса
+
+        if form.is_valid():  # если пользователь ввёл всё правильно и нигде не ошибся, то сохраняем новый товар
+            form.save()
+
+        return super().get(request, *args, **kwargs)
+
+    def news_list(request):
+        post_filter = PostFilter(request.GET, queryset=Post.objects.all())
+        return render(request, 'news.html', {filter: post_filter})
+
+
+# дженерик для получения деталей о товаре
+class PostDetailView(DetailView):
+    template_name = 'flatpages/post_detail.html'
+    queryset = Post.objects.all()
+
+
+# дженерик для создания объекта. Надо указать только имя шаблона и класс формы, который мы написали в прошлом юните. Остальное он сделает за вас
+class PostCreateView(CreateView):
+    template_name = 'flatpages/post_create.html'
+    form_class = PostForm
+
+
+# дженерик для редактирования объекта
+class PostUpdateView(UpdateView):
+    template_name = 'flatpages/post_create.html'
+    form_class = PostForm
+
+    # метод get_object мы используем вместо queryset, чтобы получить информацию об объекте, который мы собираемся редактировать
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+# дженерик для удаления товара
+class PostDeleteView(DeleteView):
+    template_name = 'flatpages/post_delete.html'
+    queryset = Post.objects.all()
+    success_url = '/news/'
